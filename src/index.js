110dcad08493986c48b979a5b2f14ab57981d1eb
KrlozMedina <charli1216@hotmail.com>
2022-12-11 05:46:34 -0500

    [DB] Comentar la conexion a MongoDB

--- src/index.js
+++ src/index.js
@@ -12,17 +12,21 @@ const port = process.env.PORT || 1234;
 
     const userMongo = 'KrlozMedina';
     const passwordMongo = 'PNj99mXR1ryVDBgz';
-    const dbname = 'laboratorio';
-    const uri = `mongodb+srv://${userMongo}:${passwordMongo}@data.6ep5zsp.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+    const DBName = 'laboratorio';
+    const uri = `mongodb+srv://${userMongo}:${passwordMongo}@data.6ep5zsp.mongodb.net/${DBName}?retryWrites=true&w=majority`;
 
-    mongoose.connect(uri)
-        .then(() => console.log('Base de datos conectada'))
-        .catch(e => console.error(e))
+    // mongoose.set('strictQuery', true);
+
+
+    // mongoose.connect(uri)
+    //     .then(() => console.log('Base de datos conectada'))
+    //     .catch(e => console.error('error', e))
 
     const user = require('../models/user')
 
-    // const tecnico = new user({ username: 'Tecnico2', password: "tecnico2022", name: "Tecnico nuevo" });
-    // tecnico.save().then(() => console.log('yo reparo'));
+    // const technical = new user({ username: 'technical2', password: "technical2022", name: "Técnico nuevo" });
+    // technical.save().then(() => console.log('yo reparo'));
+
 
     try {
         const arrayUsers = user.find()
@@ -53,20 +57,22 @@ app.use(express.json());
 
 app.post('/login', (req, res) => {
     const user = req.body.user;
-    console.log(user)
     
     if (findUser(user.username, user.password)) {
         const token = jwt.sign(user, privateKey, { expiresIn: '1h' });                
-        res.status(200).json({ token });
+        res.status(200).json({ token, name });
+        // console.log(token)
     } else {
-        res.status(200).json({ token: 'noToken' })
+        res.status(200).json({ token: 'noToken'})
     }
 });
 
 app.post('/test', (req, res) => {
     const token = req.headers['authorization'];
+    // console.log(token)
     jwt.verify(token, privateKey, (err, user) => {
         if (err) {
+            // console.log('Unauthorized')
             res.status(403)
                 .json({ msg: 'Unauthorized' })
         } else {
